b0c0cb3f7e22a400cc5d459c050318f9c0b7dfc8
jeanluc kabulu <jeanluc.kabulu@gmail.com>
2018-11-15 11:11:58 +0200

    [ ✍🏿 update ] adding PEP8 to the file
    PEP is very import

--- first_file.py
+++ first_file.py
@@ -1,28 +1,39 @@
-#A small scripts that shows the first file of any folder
-#from os import *
-#from sys import *
+"""
+A small scripts that shows the first file of any folder
+"""
+
 import os
 import sys
+
 #folder=sys.argv[1]
+
 current_path=os.getcwd()
 folders=os.listdir()
 sizes=list()
 all_files=list()
+
 for folder in folders:
+	
 	if folder=='first_file.py':
+	
 		continue
+	
 	os.chdir(folder)
 	all_files=os.listdir()
 	sizes+=[len(all_files)]
 	print(sizes)
 	os.chdir(current_path)
+
 print(folders)
 print(sys.platform)
 max_index=0
+
 for i in range(len(sizes)-1):
 	if sizes[i]<sizes[i+1]:
 		max_index=i+1
+
 print("The biggest file has got ",sizes[max_index]," files")
+
 #os.chdir(folder)
 #way=getcwd()
 #all_files=os.listdir()
